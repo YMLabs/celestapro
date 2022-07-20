02a7992a00e2a75781ca1ac1013b48d1459b18c4
AshutoshDash1999 <dashashutosh1999@gmail.com>
2022-07-20 16:04:03 +0530

    value section and minor changes

--- src/App.js
+++ src/App.js
@@ -1,18 +1,21 @@
-import React from 'react';
-import './App.css';
+import React from "react";
+import "./App.css";
 import { Routes, Route } from "react-router-dom";
 
-// pages 
-import Home from './Pages/Home';
-import Navbar from './Components/Navbar/Navbar';
+// pages
+import Home from "./Pages/Home";
+import Navbar from "./Components/Navbar/Navbar";
+import Footer from "./Components/Home/Footer/Footer";
 
 function App() {
   return (
     <>
-    <Navbar/>
+      <Navbar />
       <Routes>
-        <Route path='/' element={<Home/>}/>
+        <Route path="/" element={<Home />} />
+        <Route path="/sale" element={<Home />} />
       </Routes>
+      <Footer />
     </>
   );
 }
