dbf30d7670a5753f8efb016ebb5c8cb2112b8452
AshutoshDash1999 <dashashutosh1999@gmail.com>
2022-07-20 16:03:15 +0530

    testimonial section added

---
+++ src/Components/Home/Testimonials/TestimonialCard.js
@@ -0,0 +1,92 @@
+import React from "react";
+
+function TestimonialCard() {
+  return (
+    <div class="bg-white w-full rounded-lg p-4 m-6 shadow-md hover:shadow-xl transition ease-in-out duration-200 sm:inline-block">
+      <div class="flex items-start text-left">
+        <div class="flex-shrink-0">
+          <div class="inline-block relative">
+            <a href="#" class="block relative">
+              <img
+                alt="profile"
+                src="./assets/images/profile-avatar.jpg"
+                class="mx-auto object-cover rounded-full h-16 w-16 "
+              />
+            </a>
+          </div>
+        </div>
+        <div class="ml-6">
+          <p class="flex items-baseline">
+            <span class="text-[#50B291] font-bold">
+              A Msan
+            </span>
+            <span class="text-gray-500  ml-2 text-sm">
+              2 months ago
+            </span>
+          </p>
+          <div class="flex items-center mt-1">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="20"
+              height="20"
+              class="w-4 h-4 text-yellow-500"
+              fill="currentColor"
+              viewBox="0 0 1792 1792"
+            >
+              <path d="M1728 647q0 22-26 48l-363 354 86 500q1 7 1 20 0 21-10.5 35.5t-30.5 14.5q-19 0-40-12l-449-236-449 236q-22 12-40 12-21 0-31.5-14.5t-10.5-35.5q0-6 2-20l86-500-364-354q-25-27-25-48 0-37 56-46l502-73 225-455q19-41 49-41t49 41l225 455 502 73q56 9 56 46z"></path>
+            </svg>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="20"
+              height="20"
+              class="w-4 h-4 text-yellow-500"
+              fill="currentColor"
+              viewBox="0 0 1792 1792"
+            >
+              <path d="M1728 647q0 22-26 48l-363 354 86 500q1 7 1 20 0 21-10.5 35.5t-30.5 14.5q-19 0-40-12l-449-236-449 236q-22 12-40 12-21 0-31.5-14.5t-10.5-35.5q0-6 2-20l86-500-364-354q-25-27-25-48 0-37 56-46l502-73 225-455q19-41 49-41t49 41l225 455 502 73q56 9 56 46z"></path>
+            </svg>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="20"
+              height="20"
+              class="w-4 h-4 text-yellow-500"
+              fill="currentColor"
+              viewBox="0 0 1792 1792"
+            >
+              <path d="M1728 647q0 22-26 48l-363 354 86 500q1 7 1 20 0 21-10.5 35.5t-30.5 14.5q-19 0-40-12l-449-236-449 236q-22 12-40 12-21 0-31.5-14.5t-10.5-35.5q0-6 2-20l86-500-364-354q-25-27-25-48 0-37 56-46l502-73 225-455q19-41 49-41t49 41l225 455 502 73q56 9 56 46z"></path>
+            </svg>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="20"
+              height="20"
+              class="w-4 h-4 text-yellow-500"
+              fill="currentColor"
+              viewBox="0 0 1792 1792"
+            >
+              <path d="M1728 647q0 22-26 48l-363 354 86 500q1 7 1 20 0 21-10.5 35.5t-30.5 14.5q-19 0-40-12l-449-236-449 236q-22 12-40 12-21 0-31.5-14.5t-10.5-35.5q0-6 2-20l86-500-364-354q-25-27-25-48 0-37 56-46l502-73 225-455q19-41 49-41t49 41l225 455 502 73q56 9 56 46z"></path>
+            </svg>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="20"
+              height="20"
+              class="w-4 h-4 text-yellow-500"
+              fill="currentColor"
+              viewBox="0 0 1792 1792"
+            >
+              <path d="M1728 647q0 22-26 48l-363 354 86 500q1 7 1 20 0 21-10.5 35.5t-30.5 14.5q-19 0-40-12l-449-236-449 236q-22 12-40 12-21 0-31.5-14.5t-10.5-35.5q0-6 2-20l86-500-364-354q-25-27-25-48 0-37 56-46l502-73 225-455q19-41 49-41t49 41l225 455 502 73q56 9 56 46z"></path>
+            </svg>
+          </div>
+          <div class="mt-3">
+            <p class="mt-1 max-w-xs">
+              My first job of scanning photos at the Memories 2 Digital Photo
+              Scanning was fantastic. She completed the work quickly while I was
+              waiting. Thanks for a great service..
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default TestimonialCard;
---
+++ src/Components/Home/Testimonials/Testimonials.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import TestimonialCard from './TestimonialCard'
+
+function Testimonials() {
+  return (
+    <section className='testimonials flex mx-12'>
+        <TestimonialCard/>
+        <TestimonialCard/>
+        <TestimonialCard/>
+    </section>
+  )
+}
+
+export default Testimonials
